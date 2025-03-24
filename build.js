ff5fedb0acd3f91b1705ccf3a29b88be0d82593d
Luis <maruganluis@gmail.com>
2025-03-24 19:18:10 +0000

    sesion-7

--- build.js
+++ build.js
@@ -1,5 +1,18 @@
 async function run() {
+    const name = "Luis";
+    const lastname = "Marugan";
 
+    const obj = {
+        name: "hola",
+        age: 33,
+        folder: {
+            clave: "valor",
+        },
+
+        list: [1,2,3,4, "hola"],
+    }
+
+    console.log(name, lastname);
 }
 
 run();
